3113e66886c197f9698a3e9e3b1c1a413eea80a3
Hannah Overton <hannahoverton99@gmail.com>
2024-04-12 09:39:33 -0500

    GA 2 through task 5: complete

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -73,53 +73,109 @@ function vowelBonus(word){
 
 //  console.log(vowelBonusScorer(initialPrompt()));
 
- let newPointStructure;
+let newPointStructure = transform(oldPointStructure);
 
-let scrabbleScorer;
+
+let scrabbleScorer=
+function newScrabble(word){
+   let scores= newPointStructure;
+   word= word.toLowerCase();
+   let letterPoints= 0;
+   for(let i=0; i < word.length; i++){
+      let letter= word[i];
+      if(letter in scores){
+         letterPoints += scores[letter];
+      }
+
+   } 
+   return letterPoints;
+   }
+
 
 const scoringAlgorithms = [
-    {
+   {
       name:"Simple Scorer",
       description: "Each letter is worth 1 point",
       scorerFunction: simpleScorer},
-   {
-      name: "Bonus Vowels",
-      description: "Vowels are 3 pts, consonants are 1 pt.",
-      scorerFunction: vowelBonusScorer},
-   {
-      name: "Scrabble",
-      description: "The traditional scoring algorithm.",
-      scorerFunction: oldScrabbleScorer},
-   ];
-   
-   
-   function scorerPrompt() {
-      let word = initialPrompt();
-      let numAlgorithms = scoringAlgorithms.length;
-      let algorithmTypes ="";
+      {
+         name: "Bonus Vowels",
+         description: "Vowels are 3 pts, consonants are 1 pt.",
+         scorerFunction: vowelBonusScorer},
+         {
+            name: "New Scrabble",
+            description: "The brand new scoring algorithm.",
+            scorerFunction: scrabbleScorer},
+         ];
+         
+         
+         function scorerPrompt() {
+            let word = initialPrompt();
+            let numAlgorithms = scoringAlgorithms.length;
+            let algorithmTypes ="";
+            
+            for (let i=0; i < numAlgorithms; i++) {
+               algorithmTypes += `\n${i} - ${scoringAlgorithms[i].name}: ${scoringAlgorithms[i].description}`;
+            }
+            
+            console.log("\nChoose a scoring algorithm for your game. Enter the number for the corresponding Scorer:");
+            console.log(algorithmTypes);
+            let scorerIndex = input.question("\nYour algorithm choice is: ");
+            
+            
+            console.log("Chosen algorithm name: ", scoringAlgorithms[scorerIndex].name);
+            
+            let chosenAlgorithm = scoringAlgorithms[scorerIndex];
+            
+            
+            console.log("The total score for your word is: ", chosenAlgorithm.scorerFunction(word));
+            return chosenAlgorithm;
+            
+         }
+         
+         
+         //loop through oldPointStructure object key/value pairs change points to lower case letters add point value as value to letter keys. 
+         function transform(oldPointStructure) {
+            let newStructureObj= {};
+            for (const pointValue in oldPointStructure){
+               let letters=oldPointStructure[pointValue];
+                  for(let i=0; i < letters.length; i++){
+                     let letterpoints = letters[i];
+                     newStructureObj[letterpoints.toLowerCase()] = Number(pointValue);
+                  }
+               }
+               return newStructureObj;
+            };
+
+               // a = 1;
+               // b= 3;
+               // c= 3;
+               // d= 2;
+               // e= 1;
+               // f= 4;
+               // g= 2;
+               // h= 4;
+               // i= 1;
+               // j= 8;
+               // k= 5;
+               // l= 1;
+               // m= 3;
+               // n= 1;
+               // o= 1;
+               // p= 3;
+               // q= 10;
+               // r= 1;
+               // s= 1;
+               // t= 1;
+               // u= 1;
+               // v= 4;
+               // w= 4;
+               // x= 8;
+               // y= 4;
+               // z= 10;  
+                
+               
+         
       
-      for (let i=0; i < numAlgorithms; i++) {
-         algorithmTypes += `\n${i} - ${scoringAlgorithms[i].name}: ${scoringAlgorithms[i].description}`;
-   }
-
-   console.log("\nChoose a scoring algorithm for your game. Enter the number for the corresponding Scorer:");
-   console.log(algorithmTypes);
-   let scorerIndex = input.question("\nYour algorithm choice is: ");
-
-
-console.log("Chosen algorithm name: ", scoringAlgorithms[scorerIndex].name);
-
- let chosenAlgorithm = scoringAlgorithms[scorerIndex];
-
-
-      console.log("The total score for your word is: ", chosenAlgorithm.scorerFunction(word));
-      return chosenAlgorithm;
-      
-   }
-   
-   
-   
-   function transform() {};
    
    
    function runProgram() {
